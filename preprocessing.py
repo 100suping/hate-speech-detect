6f40b553867cc25c9379078b1ff573c6a3cf9a24
won7527 <wonheo7527@gmail.com>
2024-10-09 09:15:59 +0000

    fix

--- preprocessing.py
+++ preprocessing.py
@@ -2,16 +2,17 @@ import googletrans as gt
 import random
 from koeda import EasyDataAugmentation, AEasierDataAugmentation
 
+
 def Translator_Augmentation(text, language=None, repetition=2):
-    '''
-        input : 
-            - text 
-            - language : 언어 선택. 선택하지 않으면 랜덤
-            - repetition : default = None 
-    
-        output :
-            - List
-    '''
+    """
+    input :
+        - text
+        - language : 언어 선택. 선택하지 않으면 랜덤
+        - repetition : default = None
+
+    output :
+        - List
+    """
     translator = gt.Translator()
     result = []
     keys = list(gt.LANGUAGES.keys())
@@ -21,19 +22,28 @@ def Translator_Augmentation(text, language=None, repetition=2):
             select_language = random.choice(keys)
         else:
             select_language = language
-        translated = translator.translate(text, src='ko', dest=select_language)
-        re_translated = translator.translate(translated.text, src=select_language, dest='ko')
+        translated = translator.translate(text, src="ko", dest=select_language)
+        re_translated = translator.translate(
+            translated.text, src=select_language, dest="ko"
+        )
         result.append(re_translated.text)
-    
+
     return result
+
 
 ANALYZER = ["Okt", "Kkma", "Komoran", "Hannanum"]
 
-def AEDA_Augmentation(text, morpheme_analyzer=None, repetition=1, punctuations=[".", ",", "!", "?", ";", ":"]):
+
+def AEDA_Augmentation(
+    text,
+    morpheme_analyzer=None,
+    repetition=1,
+    punctuations=[".", ",", "!", "?", ";", ":"],
+):
     if morpheme_analyzer is None:
         morpheme_analyzer = random.choice(ANALYZER)
     aeda = AEasierDataAugmentation(
         morpheme_analyzer=morpheme_analyzer, punctuations=punctuations
     )
     result = aeda(text, repetition=repetition)
-    return(result)
+    return result
